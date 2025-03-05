c6588c0aaefd7466bdb1f0128ad04034840e8575
vishvas04 <vishvaskalyan04@gmail.com>
2025-03-05 17:55:18 +0530

    hello

--- src/main/java/com/vishvas/simpleWebApp/Controllers/ProductController.java
+++ src/main/java/com/vishvas/simpleWebApp/Controllers/ProductController.java
@@ -1,12 +1,22 @@
 package com.vishvas.simpleWebApp.Controllers;
 
+import com.vishvas.simpleWebApp.Model.Product;
+import com.vishvas.simpleWebApp.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
+
+    @Autowired
+    ProductService service;
     @RequestMapping("/product")
-    public String greet(){
-        return "Hello";
+    public List<Product> greet(){
+//        return "Hello";
+        return service.getProducts();
     }
+
 }
--- src/main/java/com/vishvas/simpleWebApp/Service/ProductService.java
+++ src/main/java/com/vishvas/simpleWebApp/Service/ProductService.java
@@ -1,4 +1,17 @@
 package com.vishvas.simpleWebApp.Service;
 
+import com.vishvas.simpleWebApp.Model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.*;
+
+@Service
 public class ProductService {
+    List<Product> products= Arrays.asList(new Product("Dove",1000,100),
+            new Product("Axe",900,987));
+
+    public List<Product> getProducts() {
+        return products;
+    }
 }
